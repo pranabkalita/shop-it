e4f36c1642fbbc1ca463e9c3ffd0282a2362b402
Pranab Kalita <pavankalita@gmail.com>
2021-08-08 10:28:38 +0530

    Catch the Mongoose Validation Error and the Invalid Mongoose ID error

--- server/middlewares/errors.js
+++ server/middlewares/errors.js
@@ -1,3 +1,5 @@
+import ErrorHandler from './../utils/errorHandler.js'
+
 export default (err, req, res, next) => {
   err.statusCode = err.statusCode || 500
   err.message = err.message || 'Internal Server Error.'
@@ -14,6 +16,18 @@ export default (err, req, res, next) => {
   if (process.env.NODE_ENV === 'PRODUCTION') {
     let error = { ...err }
     error.message = err.message
+
+    // Wrong Mongoose Object ID Error
+    if (err.name === 'CastError') {
+      const message = `Resource not found. Invalid: ${err.path}`
+      error = new ErrorHandler(message, 400)
+    }
+
+    // Handling Mongoose Validation Error
+    if (err.name === 'ValidationError') {
+      const message = Object.values(err.errors).map((value) => value.message)
+      error = new ErrorHandler(message, 400)
+    }
 
     res.status(error.statusCode).json({
       success: false,
