5bea9492e5de64ddbac3387fb5708cb9d69375c8
Pranab Kalita <pavankalita@gmail.com>
2021-08-09 22:41:41 +0530

    Show user name in header. Show conditional menu items in header. Logout user

--- client/src/store/actions/userAction.js
+++ client/src/store/actions/userAction.js
@@ -10,6 +10,8 @@ import {
   LOAD_USER_REQUEST,
   LOAD_USER_SUCCESS,
   LOAD_USER_FAIL,
+  LOGOUT_SUCCESS,
+  LOGOUT_FAIL,
   CLEAR_ERRORS,
 } from './../constants/userConstants'
 
@@ -78,6 +80,21 @@ export const loadUser = () => async (dispatch) => {
   } catch (error) {
     dispatch({
       type: LOAD_USER_FAIL,
+      payload: error.response.data.errMessage,
+    })
+  }
+}
+
+export const logout = () => async (dispatch) => {
+  try {
+    await axios.get('/api/v1/logout')
+
+    dispatch({
+      type: LOGOUT_SUCCESS,
+    })
+  } catch (error) {
+    dispatch({
+      type: LOGOUT_FAIL,
       payload: error.response.data.errMessage,
     })
   }
--- client/src/store/reducers/userReducer.js
+++ client/src/store/reducers/userReducer.js
@@ -8,6 +8,8 @@ import {
   LOAD_USER_REQUEST,
   LOAD_USER_SUCCESS,
   LOAD_USER_FAIL,
+  LOGOUT_SUCCESS,
+  LOGOUT_FAIL,
   CLEAR_ERRORS,
 } from './../constants/userConstants'
 
@@ -43,6 +45,17 @@ export const authReducer = (state = { user: {} }, action) => {
         loading: false,
         isAuthenticated: false,
         user: null,
+        error: action.payload,
+      }
+    case LOGOUT_SUCCESS:
+      return {
+        loading: false,
+        isAuthenticated: false,
+        user: null,
+      }
+    case LOGOUT_FAIL:
+      return {
+        ...state,
         error: action.payload,
       }
     case CLEAR_ERRORS:
